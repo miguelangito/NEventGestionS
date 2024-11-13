958c1288c570d49eb25c5be33dbaf48d4f25d4f3
miguelangito <miguelangelespinalospina@gmail.com>
2024-11-13 10:26:11 -0500

    bootstrap, login, logout

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem "bootsnap", require: false
 
 gem "devise", "~> 4.9", ">= 4.9.4"
 
+gem "bootstrap", "~> 5.3.3"
+
+gem "sassc-rails"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ApplicationRecord
   belongs_to :user
+  validates :date, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 3, maximum: 250 }
+  validate :date_is_not_past
+
+  private
+  def date_is_not_past
+    if date.present? && date < Date.today
+      errors.add(:date, "cannot be in the past")
+    end
+  end
 end
